12eeb7bc7e439f5ab9074363bd3ac85792826314
Esophose <esophose@gmail.com>
2019-03-08 21:54:16 -0700

    Removed Coins softdepend, Vault is now required if you are using it

--- src/main/java/me/goodandevil/skyblock/economy/EconomyManager.java
+++ src/main/java/me/goodandevil/skyblock/economy/EconomyManager.java
@@ -1,16 +1,13 @@
 package me.goodandevil.skyblock.economy;
 
+import me.goodandevil.skyblock.api.event.player.PlayerWithdrawMoneyEvent;
+import net.milkbowl.vault.economy.Economy;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.RegisteredServiceProvider;
 
-import me.goodandevil.skyblock.api.event.player.PlayerWithdrawMoneyEvent;
-import net.milkbowl.vault.economy.Economy;
-import net.nifheim.beelzebu.coins.CoinsAPI;
-
 public class EconomyManager {
 
-	private EconomyPlugin economyPlugin;
 	private Economy economy;
 
 	public EconomyManager() {
@@ -19,43 +16,24 @@ public class EconomyManager {
 
 	public void setup() {
 		if (Bukkit.getServer().getPluginManager().getPlugin("Vault") != null) {
-			RegisteredServiceProvider<Economy> registeredServiceProvider = Bukkit.getServer().getServicesManager()
-					.getRegistration(Economy.class);
+			RegisteredServiceProvider<Economy> registeredServiceProvider = Bukkit.getServer().getServicesManager().getRegistration(Economy.class);
 
-			if (registeredServiceProvider != null) {
+			if (registeredServiceProvider != null)
 				economy = registeredServiceProvider.getProvider();
-			}
-
-			economyPlugin = EconomyPlugin.Vault;
-		} else if (Bukkit.getServer().getPluginManager().getPlugin("Coins") != null) {
-			economyPlugin = EconomyPlugin.Coins;
 		}
 	}
 
 	public double getBalance(Player player) {
-		if (economy != null) {
-			return economy.getBalance(player);
-		} else if (economyPlugin == EconomyPlugin.Coins) {
-			return CoinsAPI.getCoins(player.getUniqueId());
-		}
-
-		return 0.0D;
+		return economy == null ? 0.0D : economy.getBalance(player);
 	}
 
 	public boolean hasBalance(Player player, double money) {
-		if (getBalance(player) >= money) {
-			return true;
-		}
-
-		return false;
+		return getBalance(player) >= money;
 	}
 
 	public void withdraw(Player player, double money) {
-		if (economy != null) {
+		if (economy != null)
 			economy.withdrawPlayer(player, money);
-		} else if (economyPlugin == EconomyPlugin.Coins) {
-			CoinsAPI.takeCoins(player.getUniqueId(), money);
-		}
 
 		Bukkit.getServer().getPluginManager().callEvent(new PlayerWithdrawMoneyEvent(player, money));
 	}
@@ -63,25 +41,12 @@ public class EconomyManager {
 	public void deposit(Player player, double money) {
 		if (economy != null) {
 			economy.depositPlayer(player, money);
-		} else if (economyPlugin == EconomyPlugin.Coins) {
-			CoinsAPI.addCoins(player.getUniqueId(), money);
 		}
 
 		Bukkit.getServer().getPluginManager().callEvent(new PlayerWithdrawMoneyEvent(player, money));
 	}
 
 	public boolean isEconomy() {
-		if (Bukkit.getServer().getPluginManager().getPlugin("Vault") == null
-				&& Bukkit.getServer().getPluginManager().getPlugin("Coins") == null) {
-			return false;
-		}
-
-		return true;
-	}
-
-	public enum EconomyPlugin {
-
-		Vault, Coins;
-
+		return Bukkit.getServer().getPluginManager().getPlugin("Vault") != null;
 	}
 }
