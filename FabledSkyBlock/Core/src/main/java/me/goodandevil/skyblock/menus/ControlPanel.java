8cacedb6cc256ccf6982a259d979c8e823d018fa
Brady <brady.renting@live.nl>
2019-08-15 17:45:30 +0000

    Redesign for Island CP, Added Island Lock and Upgrades to CP

--- FabledSkyBlock/Core/src/main/java/me/goodandevil/skyblock/menus/ControlPanel.java
+++ FabledSkyBlock/Core/src/main/java/me/goodandevil/skyblock/menus/ControlPanel.java
@@ -2,6 +2,7 @@ package me.goodandevil.skyblock.menus;
 
 import java.io.File;
 
+import me.goodandevil.skyblock.island.Island;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
@@ -32,6 +33,7 @@ public class ControlPanel {
 
 	public void open(Player player) {
 		SkyBlock skyblock = SkyBlock.getInstance();
+		Island island = skyblock.getIslandManager().getIsland(player);
 
 		Config config = skyblock.getFileManager().getConfig(new File(skyblock.getDataFolder(), "language.yml"));
 		FileConfiguration configLoad = config.getFileConfiguration();
@@ -43,6 +45,14 @@ public class ControlPanel {
 					&& (is.getItemMeta().getDisplayName().equals(ChatColor.translateAlternateColorCodes('&',
 							configLoad.getString("Menu.ControlPanel.Item.Teleport.Displayname"))))) {
 				Bukkit.getServer().getScheduler().runTaskLater(skyblock, () -> Bukkit.getServer().dispatchCommand(player, "island teleport"), 1L);
+			} else if ((is.getType() == Materials.IRON_DOOR.parseMaterial()) && (is.hasItemMeta())
+					&& (is.getItemMeta().getDisplayName().equals(ChatColor.translateAlternateColorCodes('&',
+					configLoad.getString("Menu.ControlPanel.Item.Lock.Displayname"))))) {
+					if (island.isOpen()) {
+						Bukkit.getServer().getScheduler().runTaskLater(skyblock, () -> Bukkit.getServer().dispatchCommand(player, "island close"), 1L);
+					} else {
+						Bukkit.getServer().getScheduler().runTaskLater(skyblock, () -> Bukkit.getServer().dispatchCommand(player, "island open"), 1L);
+					}
 			} else if ((is.getType() == Materials.BLACK_STAINED_GLASS_PANE.parseMaterial()) && (is.hasItemMeta())
 					&& (is.getItemMeta().getDisplayName().equals(ChatColor.translateAlternateColorCodes('&',
 							configLoad.getString("Menu.ControlPanel.Item.Barrier.Displayname"))))) {
@@ -76,41 +86,58 @@ public class ControlPanel {
 				Bukkit.getServer().getScheduler().runTaskLater(skyblock, () -> Bukkit.getServer().dispatchCommand(player, "island bans"), 1L);
 			} else if ((is.getType() == Materials.OAK_SIGN.parseMaterial()) && (is.hasItemMeta())
 					&& (is.getItemMeta().getDisplayName().equals(ChatColor.translateAlternateColorCodes('&',
-							configLoad.getString("Menu.ControlPanel.Item.Visitors.Displayname"))))) {
+					configLoad.getString("Menu.ControlPanel.Item.Visitors.Displayname"))))) {
 				Bukkit.getServer().getScheduler().runTaskLater(skyblock, () -> Bukkit.getServer().dispatchCommand(player, "island visitors"), 1L);
+			} else if ((is.getType() == Materials.ANVIL.parseMaterial()) && (is.hasItemMeta())
+					&& (is.getItemMeta().getDisplayName().equals(ChatColor.translateAlternateColorCodes('&',
+					configLoad.getString("Menu.ControlPanel.Item.Upgrades.Displayname"))))) {
+				Bukkit.getServer().getScheduler().runTaskLater(skyblock, () -> Bukkit.getServer().dispatchCommand(player, "island upgrades"), 1L);
 			}
 		});
 
+		// Teleport to island and open/close island
 		nInv.addItem(nInv.createItem(Materials.OAK_DOOR.parseItem(),
 				configLoad.getString("Menu.ControlPanel.Item.Teleport.Displayname"),
-				configLoad.getStringList("Menu.ControlPanel.Item.Teleport.Lore"), null, null, null), 0);
+				configLoad.getStringList("Menu.ControlPanel.Item.Teleport.Lore"), null, null, null), 1);
+		nInv.addItem(nInv.createItem(Materials.IRON_DOOR.parseItem(),
+				configLoad.getString("Menu.ControlPanel.Item.Lock.Displayname"),
+				configLoad.getStringList("Menu.ControlPanel.Item.Lock.Lore"), null, null, null), 10);
+
+		// Glass panes barriers
 		nInv.addItem(nInv.createItem(Materials.BLACK_STAINED_GLASS_PANE.parseItem(),
-				configLoad.getString("Menu.ControlPanel.Item.Barrier.Displayname"), null, null, null, null), 1);
+				configLoad.getString("Menu.ControlPanel.Item.Barrier.Displayname"), null, null, null, null), 0, 2, 5, 8, 9, 11, 14, 17);
+
+		// 4 Items at the left
 		nInv.addItem(nInv.createItem(new ItemStack(Materials.EXPERIENCE_BOTTLE.parseMaterial()),
 				configLoad.getString("Menu.ControlPanel.Item.Level.Displayname"),
-				configLoad.getStringList("Menu.ControlPanel.Item.Level.Lore"), null, null, null), 2);
+				configLoad.getStringList("Menu.ControlPanel.Item.Level.Lore"), null, null, null), 3);
 		nInv.addItem(nInv.createItem(new ItemStack(Material.NAME_TAG),
 				configLoad.getString("Menu.ControlPanel.Item.Settings.Displayname"),
-				configLoad.getStringList("Menu.ControlPanel.Item.Settings.Lore"), null, null, null), 3);
-		nInv.addItem(nInv.createItem(new ItemStack(Material.ITEM_FRAME),
-				configLoad.getString("Menu.ControlPanel.Item.Members.Displayname"),
-				configLoad.getStringList("Menu.ControlPanel.Item.Members.Lore"), null, null, null), 4);
-		nInv.addItem(nInv.createItem(Materials.OAK_SAPLING.parseItem(),
-				configLoad.getString("Menu.ControlPanel.Item.Biome.Displayname"),
-				configLoad.getStringList("Menu.ControlPanel.Item.Biome.Lore"), null, null, null), 5);
+				configLoad.getStringList("Menu.ControlPanel.Item.Settings.Lore"), null, null, null), 4);
 		nInv.addItem(nInv.createItem(Materials.CLOCK.parseItem(),
 				configLoad.getString("Menu.ControlPanel.Item.Weather.Displayname"),
-				configLoad.getStringList("Menu.ControlPanel.Item.Weather.Lore"), null, null, null), 6);
+				configLoad.getStringList("Menu.ControlPanel.Item.Weather.Lore"), null, null, null), 12);
+		nInv.addItem(nInv.createItem(Materials.OAK_SAPLING.parseItem(),
+				configLoad.getString("Menu.ControlPanel.Item.Biome.Displayname"),
+				configLoad.getStringList("Menu.ControlPanel.Item.Biome.Lore"), null, null, null), 13);
+
+		// 4 Items at the right
+		nInv.addItem(nInv.createItem(new ItemStack(Material.ITEM_FRAME),
+				configLoad.getString("Menu.ControlPanel.Item.Members.Displayname"),
+				configLoad.getStringList("Menu.ControlPanel.Item.Members.Lore"), null, null, null), 16);
 		nInv.addItem(nInv.createItem(new ItemStack(Material.IRON_AXE),
 				configLoad.getString("Menu.ControlPanel.Item.Bans.Displayname"),
 				configLoad.getStringList("Menu.ControlPanel.Item.Bans.Lore"), null, null,
-				new ItemFlag[] { ItemFlag.HIDE_ATTRIBUTES }), 7);
+				new ItemFlag[] { ItemFlag.HIDE_ATTRIBUTES }), 6);
 		nInv.addItem(nInv.createItem(new ItemStack(Materials.OAK_SIGN.parseMaterial()),
 				configLoad.getString("Menu.ControlPanel.Item.Visitors.Displayname"),
-				configLoad.getStringList("Menu.ControlPanel.Item.Visitors.Lore"), null, null, null), 8);
+				configLoad.getStringList("Menu.ControlPanel.Item.Visitors.Lore"), null, null, null), 7);
+		nInv.addItem(nInv.createItem(new ItemStack(Materials.ANVIL.parseMaterial()),
+				configLoad.getString("Menu.ControlPanel.Item.Upgrades.Displayname"),
+				configLoad.getStringList("Menu.ControlPanel.Item.Upgrades.Lore"), null, null, null), 15);
 
 		nInv.setTitle(ChatColor.translateAlternateColorCodes('&', configLoad.getString("Menu.ControlPanel.Title")));
-		nInv.setRows(1);
+		nInv.setRows(2);
 
 		Bukkit.getServer().getScheduler().runTask(skyblock, () -> nInv.open());
 	}
