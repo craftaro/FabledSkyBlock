9a3b800f5fc0f621ffd1a109c326032253f1fbec
Fabrizio La Rosa <lr.fabrizio@gmail.com>
2020-07-03 14:47:03 +0200

    Added more APIs for levels

--- src/main/java/com/songoda/skyblock/api/levelling/LevellingManager.java
+++ src/main/java/com/songoda/skyblock/api/levelling/LevellingManager.java
@@ -1,8 +1,12 @@
 package com.songoda.skyblock.api.levelling;
 
 import com.google.common.base.Preconditions;
+import com.songoda.core.compatibility.CompatibleMaterial;
 import com.songoda.skyblock.api.island.Island;
 import com.songoda.skyblock.levelling.IslandLevelManager;
+import com.songoda.skyblock.levelling.calculator.Calculator;
+import com.songoda.skyblock.levelling.calculator.CalculatorRegistry;
+import org.bukkit.Material;
 
 public class LevellingManager {
 
@@ -18,5 +22,16 @@ public class LevellingManager {
     public void calculatePoints(Island island) {
         Preconditions.checkArgument(island != null, "Cannot calculate points to null island");
         this.levellingManager.startScan(null, island.getIsland());
+    }
+    
+    /**
+     * Register a new material calculator for a determined material
+     * @param calculator
+     * @param material
+     */
+    public void registerCalculator(Calculator calculator, Material material){
+        Preconditions.checkArgument(calculator != null, "Cannot use a null calculator");
+        Preconditions.checkArgument(material != null, "Cannot use a null material");
+        CalculatorRegistry.registerCalculator(calculator, CompatibleMaterial.getMaterial(material));
     }
 }
