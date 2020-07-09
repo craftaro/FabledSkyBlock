d078b5e2d42a960a5049943c654b4d103fbbd044
Fabrizio La Rosa <lr.fabrizio@gmail.com>
2020-07-09 13:31:42 +0200

    Make Manager constructor public

--- src/main/java/com/songoda/skyblock/manager/Manager.java
+++ src/main/java/com/songoda/skyblock/manager/Manager.java
@@ -6,7 +6,7 @@ public abstract class Manager {
     
     protected SkyBlock plugin;
     
-    Manager(SkyBlock plugin) {
+    public Manager(SkyBlock plugin) {
         this.plugin = plugin;
     }
     
