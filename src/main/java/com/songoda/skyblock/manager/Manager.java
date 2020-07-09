dc315106bfbcbcbe4816f0f568a5c3571b562667
Fabrizio La Rosa <lr.fabrizio@gmail.com>
2020-07-09 08:33:39 +0200

    Create Manager class

---
+++ src/main/java/com/songoda/skyblock/manager/Manager.java
@@ -0,0 +1,23 @@
+package com.songoda.skyblock.manager;
+
+import com.songoda.skyblock.SkyBlock;
+
+public abstract class Manager {
+    
+    protected SkyBlock plugin;
+    
+    Manager(SkyBlock plugin) {
+        this.plugin = plugin;
+    }
+    
+    /**
+     * Reloads the Manager's settings
+     */
+    public abstract void reload();
+    
+    /**
+     * Cleans up the Manager's resources
+     */
+    public abstract void disable();
+    
+}
