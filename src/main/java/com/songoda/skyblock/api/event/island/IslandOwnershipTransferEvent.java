a6728d95a4c7d1f0a463d767a384542a71eca9c7
Geik <ben@poyrazinan.com.tr>
2021-06-12 12:11:01 +0300

    oldOwner

--- src/main/java/com/songoda/skyblock/api/event/island/IslandOwnershipTransferEvent.java
+++ src/main/java/com/songoda/skyblock/api/event/island/IslandOwnershipTransferEvent.java
@@ -9,16 +9,22 @@ public class IslandOwnershipTransferEvent extends IslandEvent {
     private static final HandlerList HANDLERS = new HandlerList();
 
     private final OfflinePlayer owner;
+    private final OfflinePlayer oldOwner;
 
-    public IslandOwnershipTransferEvent(Island island, OfflinePlayer owner) {
+    public IslandOwnershipTransferEvent(Island island, OfflinePlayer owner, OfflinePlayer oldOwner) {
         super(island);
         this.owner = owner;
+        this.oldOwner = oldOwner;
     }
 
     public static HandlerList getHandlerList() {
         return HANDLERS;
     }
 
+    public OfflinePlayer getOldOwner() {
+        return oldOwner;   
+    }
+    
     public OfflinePlayer getOwner() {
         return owner;
     }
