700b293f9c90858dfadb1fc118d9808face3d47e
Christian Koop <contact@sprax2013.de>
2023-08-15 19:02:31 +0200

    Updates UltimateStacker Dependency+Implementation to API v1.0.0-SNAPSHOT

--- src/main/java/com/craftaro/skyblock/levelling/calculator/impl/UltimateStackerCalculator.java
+++ src/main/java/com/craftaro/skyblock/levelling/calculator/impl/UltimateStackerCalculator.java
@@ -1,18 +1,24 @@
 package com.craftaro.skyblock.levelling.calculator.impl;
 
 import com.craftaro.skyblock.levelling.calculator.SpawnerCalculator;
-import com.songoda.ultimatestacker.UltimateStacker;
-import com.songoda.ultimatestacker.stackable.spawner.SpawnerStack;
+import com.craftaro.ultimatestacker.api.UltimateStackerApi;
+import com.craftaro.ultimatestacker.api.stack.spawner.SpawnerStack;
+import org.bukkit.Bukkit;
 import org.bukkit.block.CreatureSpawner;
+import org.bukkit.plugin.Plugin;
 
 public class UltimateStackerCalculator implements SpawnerCalculator {
     @Override
     public long getSpawnerAmount(CreatureSpawner spawner) {
-        if (!UltimateStacker.getInstance().getConfig().getBoolean("Spawners.Enabled")) {
+        if (!getUltimateStackerPlugin().getConfig().getBoolean("Spawners.Enabled")) {
             return 0;
         }
 
-        final SpawnerStack stack = UltimateStacker.getInstance().getSpawnerStackManager().getSpawner(spawner.getLocation());
+        final SpawnerStack stack = UltimateStackerApi.getSpawnerStackManager().getSpawner(spawner.getLocation());
         return stack == null ? 0 : stack.getAmount();
+    }
+
+    private Plugin getUltimateStackerPlugin() {
+        return Bukkit.getPluginManager().getPlugin("UltimateStacker");
     }
 }
